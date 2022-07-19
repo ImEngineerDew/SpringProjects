666eccb3f8c9b4973b128965bd25b39bb7874722
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-07-18 21:50:20 -0500

    Ordering the taco App

---
+++ taco-cloud/src/main/java/com/toadsdewin/tacocloud/Taco.java
@@ -0,0 +1,11 @@
+package com.toadsdewin.tacocloud;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Taco
+{
+    private String name;
+    private List<Ingredient> ingredientList;
+
+}
