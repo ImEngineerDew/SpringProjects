404e8b3855d364fa41b16779e95e1d0596cb2411
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-07-18 21:36:33 -0500

    A minor change on this project

--- taco-cloud/src/test/java/com/toadsdewin/tacocloud/TacoControllerTest.java
+++ taco-cloud/src/test/java/com/toadsdewin/tacocloud/TacoControllerTest.java
@@ -1,5 +1,4 @@
-package tacos;
-
+package com.toadsdewin.tacocloud;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
