b38778afc54ef324ff9c292da3480355558b44e5
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-07-28 20:49:15 -0500

    Working on TacoOrder.java

--- taco-cloud/src/main/java/com/toadsdewin/tacocloud/Files/TacoOrder.java
+++ taco-cloud/src/main/java/com/toadsdewin/tacocloud/Files/TacoOrder.java
@@ -1,8 +1,24 @@
 package com.toadsdewin.tacocloud.Files;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class TacoOrder
 {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
 
+    private List<Taco> tacos = new ArrayList<>(); /*Using the Taco.java to insert your taco order*/
+    public void addTaco(Taco taco)
+    {
+        this.tacos.add(taco);
+    }
 }
