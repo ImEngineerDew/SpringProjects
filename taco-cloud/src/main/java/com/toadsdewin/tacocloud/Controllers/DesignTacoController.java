f8214b73e43b8ad8fdb9d2114fbd19cc2718369c
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-08-01 17:00:31 -0500

    Creating the new controllers for manage the taco orders

--- taco-cloud/src/main/java/com/toadsdewin/tacocloud/Controllers/DesignTacoController.java
+++ taco-cloud/src/main/java/com/toadsdewin/tacocloud/Controllers/DesignTacoController.java
@@ -8,10 +8,7 @@ import com.toadsdewin.tacocloud.Files.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -66,6 +63,15 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder)
+    {
+        tacoOrder.addTaco(taco);
+        log.info("Procesando taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 }
 
