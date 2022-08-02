339e228600f0b7ad7cdc46abe8462da12014da16
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-08-02 18:15:38 -0500

    Put the validations

--- taco-cloud/src/main/java/com/toadsdewin/tacocloud/Files/Taco.java
+++ taco-cloud/src/main/java/com/toadsdewin/tacocloud/Files/Taco.java
@@ -1,11 +1,18 @@
 package com.toadsdewin.tacocloud.Files;
 import lombok.Data;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
+
 
 @Data
 public class Taco
 {
+    @NotNull
+    @Size(min=5,message="El nombre debe contener al menos 5 caracteres")
     private String name;
+
+    @NotNull
+    @Size(min=1,message="Usted debe escoger sólo 1 ingrediente")
     private List<Ingredient> ingredients; /*Using the ingredients for create a taco*/
 }
