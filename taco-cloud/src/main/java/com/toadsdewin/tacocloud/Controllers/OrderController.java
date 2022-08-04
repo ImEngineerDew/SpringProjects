72567c66f73218688ad6546a5ada010c96fcf44b
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-08-04 15:39:29 -0500

    Completed the Validations

--- taco-cloud/src/main/java/com/toadsdewin/tacocloud/Controllers/OrderController.java
+++ taco-cloud/src/main/java/com/toadsdewin/tacocloud/Controllers/OrderController.java
@@ -3,11 +3,14 @@ package com.toadsdewin.tacocloud.Controllers;
 import com.toadsdewin.tacocloud.Files.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,8 +25,12 @@ public class OrderController
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus)
+    public String processOrder(@Valid Errors errors, TacoOrder order, SessionStatus sessionStatus)
     {
+        if(errors.hasErrors())
+        {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
