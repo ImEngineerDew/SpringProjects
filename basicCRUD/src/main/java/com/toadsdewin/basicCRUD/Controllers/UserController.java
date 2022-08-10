75dbb2466ce9c8524f87850f12235910f1263cbf
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-08-10 15:11:39 -0500

    Create the basic controller

---
+++ basicCRUD/src/main/java/com/toadsdewin/basicCRUD/Controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.toadsdewin.basicCRUD.Controllers;
+import com.toadsdewin.basicCRUD.Models.UserModel;
+import com.toadsdewin.basicCRUD.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/demo")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public String addNewUser(@RequestParam String name, @RequestParam String email) {
+        UserModel user = new UserModel();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+
+        return "User saved";
+    }
+
+    public Iterable<UserModel> getAllUsers()
+    {
+        return userRepository.findAll();
+    }
+}
--- basicCRUD/src/main/java/com/toadsdewin/basicCRUD/Repository/UserRepository.java
+++ basicCRUD/src/main/java/com/toadsdewin/basicCRUD/Repository/UserRepository.java
@@ -2,5 +2,4 @@ package com.toadsdewin.basicCRUD.Repository;
 import com.toadsdewin.basicCRUD.Models.UserModel;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserModel,Integer> {
-}
+public interface UserRepository extends CrudRepository<UserModel,Integer> {}
