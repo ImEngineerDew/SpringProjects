69925a5996c095a3002b8da380fd312ff231502f
Toads Dewin <76026848+ImEngineerDew@users.noreply.github.com>
2022-08-10 14:55:38 -0500

    Create the Model and their getters

---
+++ basicCRUD/src/main/java/com/toadsdewin/basicCRUD/Models/UserModel.java
@@ -0,0 +1,33 @@
+package com.toadsdewin.basicCRUD.Models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserModel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+    public String name;
+    public String email;
+
+    /***Getters***/
+    public Integer getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
+    /***Setters***/
+
+
+}
