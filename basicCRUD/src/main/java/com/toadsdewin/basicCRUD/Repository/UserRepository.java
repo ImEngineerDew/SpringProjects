8bea3d8f09b48efd82952aebbd990ca54aabbe7d
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-08-14 18:40:48 -0500

    Removed basicCRUD because a MojoException

--- basicCRUD/src/main/java/com/toadsdewin/basicCRUD/Repository/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.toadsdewin.basicCRUD.Repository;
-import com.toadsdewin.basicCRUD.Models.UserModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserModel,Integer> {}
