0e0d3201cbd6a815a48ee1d37e0aa43f9455b1be
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-08-19 15:13:21 -0500

    Create the Spring boot app with Platzi day

---
+++ platzispringboot/src/main/java/com/toadsdewin/platzispringboot/PlatzispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.toadsdewin.platzispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzispringbootApplication.class, args);
+	}
+
+}
---
+++ platzispringboot/src/test/java/com/toadsdewin/platzispringboot/PlatzispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toadsdewin.platzispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzispringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
