7e07ce2750d238e1aa3833381d48294f62b58e80
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-08-19 17:25:11 -0500

    Create the functions of the UserService

--- platzispringboot/src/main/java/com/toadsdewin/platzispringboot/Repository/UserRepository.java
+++ platzispringboot/src/main/java/com/toadsdewin/platzispringboot/Repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.toadsdewin.platzispringboot.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>
+{
+    public Optional<User> findById(Long id);
+    public ArrayList<User> findByName(String name);
 }
--- platzispringboot/src/main/java/com/toadsdewin/platzispringboot/Service/UserService.java
+++ platzispringboot/src/main/java/com/toadsdewin/platzispringboot/Service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserService
@@ -19,5 +20,24 @@ public class UserService
     public User saveUser(User user)
     {
         return userRepository.save(user);
+    }
+    public Optional<User> findById(Long id)
+    {
+        return userRepository.findById(id);
+    }
+    public ArrayList<User> findByName(String name)
+    {
+        return userRepository.findByName(name);
+    }
+    public boolean deleteById(Long id)
+    {
+        try
+        {
+            userRepository.deleteById(id);
+            return true;
+        }catch(Exception error)
+        {
+            return false;
+        }
     }
 }
