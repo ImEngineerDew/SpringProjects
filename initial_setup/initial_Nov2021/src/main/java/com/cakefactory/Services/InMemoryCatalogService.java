d46991204a37029d2827c0d1aecc4badc50e482f
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-09-06 15:40:45 -0500

    Let's run to the Gradle project

--- initial_setup/initial_Nov2021/src/main/java/com/cakefactory/Services/InMemoryCatalogService.java
+++ initial_setup/initial_Nov2021/src/main/java/com/cakefactory/Services/InMemoryCatalogService.java
@@ -1,8 +1,11 @@
 package com.cakefactory.Services;
 import com.cakefactory.Models.Item;
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 import java.util.Arrays;
 
+@Component
 public class InMemoryCatalogService implements CatalogService{
     @Override
     public Iterable<Item> getItems() {
