10d9b33bab6b5b1734a325038e2515a54508e949
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-09-06 21:36:47 -0500

    New changes

--- basicCrud/src/main/java/com/toadsdewin/basicCrud/BasicCrudApplication.java
+++ basicCrud/src/main/java/com/toadsdewin/basicCrud/BasicCrudApplication.java
@@ -2,12 +2,32 @@ package com.toadsdewin.basicCrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+@RestController
 @SpringBootApplication
 public class BasicCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicCrudApplication.class, args);
+	}|
+	@GetMapping
+	public String welcomeMessage()
+	{
+		return "Welcome to the basicCRUD!";
 	}
-
 }
+
+	/**
+	@GetMapping(path = "/laconchadelalora")
+	public List<String> message()
+	{
+		return List.of("La","Concha","de","la","lora");
+	}
+	**/
+
+
--- basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserServiceInterface.java
+++ basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserServiceInterface.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Optional;
 
-@Service
+
 public interface UserServiceInterface {
     public ArrayList<UserModel>getUsers();
     public UserModel saveUser(UserModel user);
