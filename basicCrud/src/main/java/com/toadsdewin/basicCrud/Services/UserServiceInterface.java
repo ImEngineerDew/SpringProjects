b495e9cf48b0cf42ae9a3e9c6717244a75c71fcf
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-09-09 15:26:43 -0500

    Creating the comments for the UserServiceInterface

--- basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserServiceInterface.java
+++ basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserServiceInterface.java
@@ -6,9 +6,13 @@ import java.util.Optional;
 
 
 public interface UserServiceInterface {
+    /**This method can get all the users from the DB**/
     public ArrayList<UserModel>getUsers();
+    /**This method can save each user for the DB**/
     public UserModel saveUser(UserModel user);
+    /**This method can upgrade an existent user**/
     public UserModel upgradeUser(UserModel user);
+    /**This method can get a specific user from the DB**/
     public Optional<UserModel> getById(Long id);
     public ArrayList<UserModel> getByCountry(String country);
     public ArrayList<UserModel>getByRol(String rol);
