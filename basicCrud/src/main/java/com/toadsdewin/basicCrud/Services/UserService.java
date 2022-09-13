9ec1ee9b78a56b699b1270c79dc28b910f1a7203
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-09-13 11:52:46 -0500

    Welcome back

--- basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserService.java
+++ basicCrud/src/main/java/com/toadsdewin/basicCrud/Services/UserService.java
@@ -2,8 +2,6 @@ package com.toadsdewin.basicCrud.Services;
 import com.toadsdewin.basicCrud.Models.UserModel;
 import com.toadsdewin.basicCrud.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Objects;
