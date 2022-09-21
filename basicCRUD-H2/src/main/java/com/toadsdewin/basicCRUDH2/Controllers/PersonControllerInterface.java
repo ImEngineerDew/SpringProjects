01314781132ed7fb32661c735b9ccede530ab43a
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-09-21 16:43:59 -0500

    Adding the dependency for validations and put their validations (viceversa)

--- basicCRUD-H2/src/main/java/com/toadsdewin/basicCRUDH2/Controllers/PersonControllerInterface.java
+++ basicCRUD-H2/src/main/java/com/toadsdewin/basicCRUDH2/Controllers/PersonControllerInterface.java
@@ -2,6 +2,8 @@ package com.toadsdewin.basicCRUDH2.Controllers;
 import com.toadsdewin.basicCRUDH2.Models.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.ArrayList;
 
 public interface PersonControllerInterface
@@ -11,9 +13,9 @@ public interface PersonControllerInterface
     @GetMapping(path="/{id}")
     public ResponseEntity<?> getUserById(@PathVariable("id") Long id);
     @PostMapping(path = "/add")
-    public ResponseEntity<Person> savePerson(@RequestBody Person person);
+    public ResponseEntity<Person> savePerson(@Valid @RequestBody Person person);
     @PutMapping(path ="/add/{id}")
-    public Person upgradePerson(@RequestBody Person person, @RequestBody @PathVariable("id") Long id);
+    public Person upgradePerson(@Valid @RequestBody Person person,  @Valid @RequestBody @PathVariable("id") Long id);
     @DeleteMapping(path="/{id}")
     public ResponseEntity<String>deleteById(@PathVariable("id") Long id);
     @RequestMapping(value = "/query", params = "phone")
