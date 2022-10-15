ccf277d4cefef98ff55b24a45578ed429bc601d1
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-10-14 19:29:32 -0500

    First commit on a new SpringBoot project with Gradle

---
+++ Hospital-gradle/src/main/java/com/toadsdewin/Hospital/DoctorController/DoctorController.java
@@ -0,0 +1,16 @@
+package com.toadsdewin.Hospital.DoctorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/doctor")
+public class DoctorController
+{
+    @GetMapping(path = "/")
+    public ResponseEntity<String> answerPage()
+    {
+        return ResponseEntity.ok("The webpage has been loaded");
+    }
+}
