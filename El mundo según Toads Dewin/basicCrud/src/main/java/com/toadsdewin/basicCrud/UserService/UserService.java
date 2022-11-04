f4fedb64e2f1d07299d9167eb6eff504a5bceebf
ImEngineerDew <dewin.acosta9207@gmail.com>
2022-11-03 20:52:29 -0500

    New folder for my YouTube Channel

---
+++ El mundo según Toads Dewin/basicCrud/src/main/java/com/toadsdewin/basicCrud/UserService/UserService.java
@@ -0,0 +1,78 @@
+package com.toadsdewin.basicCrud.UserService;
+import com.toadsdewin.basicCrud.UserModel.UserModel;
+import com.toadsdewin.basicCrud.UserRepository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService  implements  UserServiceInterface
+{
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserModel saveUser(UserModel user) {
+        if(user.getId()==null)
+        {
+            return userRepository.save(user);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public UserModel upgradeUser(UserModel user, Long id)
+    {
+        return null;
+    }
+
+    @Override
+    public UserModel getById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<UserModel> getByName(String name) {
+        return userRepository.findByName(name);
+    }
+
+    @Override
+    public List<UserModel> getBySurname(String surname) {
+        return userRepository.findBySurname(surname);
+    }
+
+    @Override
+    public List<UserModel> getByOccupation(String occupation) {
+        return userRepository.findByOccupation(occupation);
+    }
+
+    @Override
+    public List<UserModel> getByProfession(String profession) {
+        return userRepository.findByProfession(profession);
+    }
+
+    @Override
+    public List<UserModel> getByAge(Integer age) {
+        return userRepository.findByAge(age);
+    }
+    @Override
+    public boolean deleteUserById(Long id) {
+        try
+        {
+            userRepository.deleteById(id);
+            return true;
+        }catch(Exception error)
+        {
+            error.getMessage();
+        }
+        return false;
+    }
+}
---
+++ El mundo según Toads Dewin/basicCrud/src/main/java/com/toadsdewin/basicCrud/UserService/UserServiceInterface.java
@@ -0,0 +1,17 @@
+package com.toadsdewin.basicCrud.UserService;
+import com.toadsdewin.basicCrud.UserModel.UserModel;
+import java.util.List;
+
+public interface UserServiceInterface
+{
+    public UserModel saveUser(UserModel user);
+    public UserModel upgradeUser(UserModel user, Long id);
+    public UserModel getById(Long id);
+    public List<UserModel> getAllUsers();
+    public List<UserModel> getByName(String name);
+    public List<UserModel> getBySurname(String surname);
+    public List<UserModel> getByOccupation(String occupation);
+    public List<UserModel> getByProfession(String profession);
+    public List<UserModel> getByAge(Integer age);
+    public boolean deleteUserById(Long id);
+}
