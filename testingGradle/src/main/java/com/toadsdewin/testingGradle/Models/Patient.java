666e57ff00d023b21700b90f5485183bc5f72f51
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-08 23:43:06 -0500

    Second milestone, I had created the patient table, before to restoring the Doctor table

--- testingGradle/src/main/java/com/toadsdewin/testingGradle/Models/Patient.java
+++ testingGradle/src/main/java/com/toadsdewin/testingGradle/Models/Patient.java
@@ -1,18 +1,18 @@
 package com.toadsdewin.testingGradle.Models;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name = "patient")
+@Table(name = "Patient")
 public class Patient extends Model
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(nullable = false, length = 10)
-    private Long patientID;
+    @Column(unique = true, nullable = false, length = 20)
+    private Long idPatient;
 
-    @Column(name= "Especialización", nullable = false, length = 25)
-    private String specialization;
-
-    @Column(name = "Departamento de origen", nullable = false, length = 25)
+    @Column(name = "Departamento",nullable = false,length = 50)
     private String provinceOfOrigin;
+    @Column(name = "Especialidad",nullable = false,length = 50)
+    private String specialization;
 }
