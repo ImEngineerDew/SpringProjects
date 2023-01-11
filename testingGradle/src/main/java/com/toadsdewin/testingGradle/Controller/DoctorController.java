17c1439f06b644d1984dd5d1fb0102f9d8456099
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-10 22:04:13 -0500

    This is the 7th day of 30 day challenge code

---
+++ testingGradle/src/main/java/com/toadsdewin/testingGradle/Controller/DoctorController.java
@@ -0,0 +1,10 @@
+package com.toadsdewin.testingGradle.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/doctor")
+public class DoctorController
+{
+}
--- testingGradle/src/main/java/com/toadsdewin/testingGradle/Repositories/DoctorRepository.java
+++ testingGradle/src/main/java/com/toadsdewin/testingGradle/Repositories/DoctorRepository.java
@@ -1,6 +1,5 @@
 package com.toadsdewin.testingGradle.Repositories;
 import com.toadsdewin.testingGradle.Models.Doctor;
-import com.toadsdewin.testingGradle.Models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
